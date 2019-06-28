97c37930913f5ded71c2cfd45a2cf93c7149744e
qthom <q.thomas@hotmail.fr>
2019-06-28 10:19:17 +0200

    Update pie_plot.R

--- pie_plot.R
+++ pie_plot.R
@@ -4,45 +4,45 @@
 genes <- genes(txdb)
 
 gr <- gr_hen2
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_orf_simplified.R")
+source("sepclusters_orf_simplified.R")
 hen2_list <- c(l_over_orf,l_Ntrunca,l_Ctrunca,l_within,l_antisens,l_intergenic,l_2ormore)
 gr <- gr_hen2
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_overlapping_orf.R")
+source("sepclusters_overlapping_orf.R")
 hen2orf_list <- c(l_o1,l_o2,l_o3,l_o4,l_o5,l_o6,l_o7,l_o8,l_o9)
 
 gr <- gr_wt
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_orf_simplified.R")
+source("sepclusters_orf_simplified.R")
 Col0_list <- c(l_over_orf,l_Ntrunca,l_Ctrunca,l_within,l_antisens,l_intergenic,l_2ormore)
 gr <- gr_wt
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_overlapping_orf.R")
+source("sepclusters_overlapping_orf.R")
 Col0orf_list <- c(l_o1,l_o2,l_o3,l_o4,l_o5,l_o6,l_o7,l_o8,l_o9)
 
 gr <- gr_hen2cold
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_orf_simplified.R")
+source("sepclusters_orf_simplified.R")
 hen2cold_list <- c(l_over_orf,l_Ntrunca,l_Ctrunca,l_within,l_antisens,l_intergenic,l_2ormore)
 gr <- gr_hen2cold
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_overlapping_orf.R")
+source("sepclusters_overlapping_orf.R")
 hen2coldorf_list <- c(l_o1,l_o2,l_o3,l_o4,l_o5,l_o6,l_o7,l_o8,l_o9)
 
 gr <- gr_wtcold
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_orf_simplified.R")
+source("sepclusters_orf_simplified.R")
 Col0cold_list <- c(l_over_orf,l_Ntrunca,l_Ctrunca,l_within,l_antisens,l_intergenic,l_2ormore)
 gr <- gr_wtcold
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_overlapping_orf.R")
+source("sepclusters_overlapping_orf.R")
 Col0coldorf_list <- c(l_o1,l_o2,l_o3,l_o4,l_o5,l_o6,l_o7,l_o8,l_o9)
 
 gr <- gr_ssrp1
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_orf_simplified.R")
+source("sepclusters_orf_simplified.R")
 ssrp1_list <- c(l_over_orf,l_Ntrunca,l_Ctrunca,l_within,l_antisens,l_intergenic,l_2ormore)
 gr <- gr_ssrp1
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_overlapping_orf.R")
+source("sepclusters_overlapping_orf.R")
 ssrp1orf_list <- c(l_o1,l_o2,l_o3,l_o4,l_o5,l_o6,l_o7,l_o8,l_o9)
 
 gr <- gr_spt16
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_orf_simplified.R")
+source("sepclusters_orf_simplified.R")
 spt16_list <- c(l_over_orf,l_Ntrunca,l_Ctrunca,l_within,l_antisens,l_intergenic,l_2ormore)
 gr <- gr_spt16
-source("/home/bcm215/Desktop/Quentin/scripts/TIF-Seq_2019/sepclusters_overlapping_orf.R")
+source("sepclusters_overlapping_orf.R")
 spt16orf_list <- c(l_o1,l_o2,l_o3,l_o4,l_o5,l_o6,l_o7,l_o8,l_o9)
 
 
@@ -62,7 +62,6 @@ dforf$log2FC_COL0cold_Hen2cold <- log(dforf$Hen2_cold,2) - log(dforf$Col0_cold,2
 dforf$log2FC_COL0_SSRP1 <- log(dforf$SSRP1,2) - log(dforf$Col0,2)
 dforf$log2FC_COL0_SPT16 <- log(dforf$SPT16,2) - log(dforf$Col0,2)
 
-setwd("/home/bcm215/Desktop/Quentin/TIF-Seq/")
 write.table(df,file="FoldChange_TUs.csv",row.names=TRUE, col.names=TRUE)
 
 p <- ggplot(data=df, aes(x=rownames(df), y=log2FC_COL0_Hen2), fill=log2FC_COL0_Hen2) +
